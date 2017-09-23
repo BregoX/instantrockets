77f63754e1b2f9df99eceddc9838c85d492dd4c9
Pavel Zavoiskikh <hapass@ya.ru>
2017-09-23 22:31:38 +0300

    Added type definitions for mocha and chai.

--- src/test/ts/AnotherTest.ts
+++ src/test/ts/AnotherTest.ts
@@ -1,12 +1,11 @@
 import 'reflect-metadata';
 import { Pipe } from '../../main/ts/model/station/Pipe';
-import { describe, it } from 'mocha';
-import * as Assertion from 'chai';
+import { expect } from 'chai';
 
 describe('My another context', () => {
     describe('Some other event', () => {
         it('should result in something else', () => {
-            Assertion.expect(true).to.eq(true);
+            expect(true).to.equal(true);
         });
     });
 });
--- src/test/ts/PipeTest.ts
+++ src/test/ts/PipeTest.ts
@@ -1,18 +1,17 @@
 import 'reflect-metadata';
 import { Pipe } from '../../main/ts/model/station/Pipe';
-import { describe, it } from 'mocha';
 import { EventDispatcherMock } from './mock/EventDispatcherMock';
-import * as Assertion from 'chai';
+import { expect } from 'chai';
 
 describe('My context', () => {
     describe('Some event', () => {
         it('should result in something', () => {
             let pipe = new Pipe(10, 20, 0, 0, new EventDispatcherMock());
 
-            Assertion.expect(pipe.x).to.eq(10);
-            Assertion.expect(pipe.y).to.eq(20);
-            Assertion.expect(pipe.i).to.eq(0);
-            Assertion.expect(pipe.j).to.eq(0);
+            expect(pipe.x).to.equal(10);
+            expect(pipe.y).to.equal(20);
+            expect(pipe.i).to.equal(0);
+            expect(pipe.j).to.equal(0);
         });
     });
 });
